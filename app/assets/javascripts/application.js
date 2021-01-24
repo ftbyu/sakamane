06580be09ebe884de19ae86d5d4e147f1b9a3df6
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-01-24 12:00:46 +0900

    Add bootstrap_template

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require rails-ujs
+//= require turbolinks
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require activestorage
+//= require bootstrap-sprockets
+//= require_tree .
